ec44a07925a5f34e329610790d3b1cec085c1e67
Douglas Phanstiel <dphansti@wire152019015063.med.unc.edu>
2019-02-19 14:43:55 -0500

    Added 'usethis' and 'testthat' functions to test slop

---
+++ tests/testthat/test-slop.R
@@ -0,0 +1,12 @@
+context("test-slop")
+
+test_that("multiplication works", {
+  A.bed = data.frame(chrom="chr1",
+                     start=15,
+                     end=20)
+  genome = data.frame(chrom=c("chr1","chr2"),size=c(1000,5000))
+  B.bed = data.frame(V1="chr1",
+                     V2=10,
+                     V3=25)
+  expect_equal(bedtoolsr::slop(A.bed,b=5,g=genome), B.bed)
+})
