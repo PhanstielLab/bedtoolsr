5e04a5b56f3d4ed7351a3221acc1fec62851c82d
Douglas Phanstiel <dphansti@wire152019015063.med.unc.edu>
2019-02-19 15:44:51 -0500

    Added 'usethis' and 'testthat' functions to test jaccard

---
+++ tests/testthat/test-jaccard.R
@@ -0,0 +1,20 @@
+context("test-jaccard")
+
+test_that("jaccard works", {
+  A.bed = data.frame(chrom = c("chr1","chr1"),
+                     start = c(10,30),
+                     end   = c(20,40))
+  
+  B.bed = data.frame(chrom = "chr1",
+                     start = 15,
+                     end   = 20)
+  C.bed = data.frame("intersection"=5,
+                     "union.intersection"=20,
+                     "jaccard" = 0.25,
+                     "n_intersections" = 1)
+  
+  
+  
+  
+  expect_equal(bedtoolsr::jaccard(a = A.bed, b = B.bed), C.bed)
+})
