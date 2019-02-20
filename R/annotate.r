660a6fa73bf330cf3a3c2bded15c5006e6e6645c
Douglas Phanstiel <dphansti@wire152019015063.med.unc.edu>
2019-02-20 12:47:30 -0500

    edited to make use of helper functions

--- R/annotate.r
+++ R/annotate.r
@@ -22,71 +22,29 @@
 #' 
 annotate <- function(i, files, S = NULL, both = NULL, counts = NULL, s = NULL, names = NULL)
 { 
+  # Required Inputs
+  i = establishpaths(input=i,name="i")
+  files = establishpaths(input=files,name="files")
+  
+  # Options
+  options = createOptions(names = c("S","both","counts","s","names"),
+                          values= c(S,both,counts,s,names))
+  
+  # Establish output file 
+  tempfile = tempfile("bedtoolsr", fileext=".txt")
+  bedtools.path <- getOption("bedtools.path")
+  if(!is.null(bedtools.path)) bedtools.path <- paste0(bedtools.path, "/")
+  cmd = paste0(bedtools.path, "bedtools annotate ", options, " -i ", i[[1]], " -files ", files[[1]], " > ", tempfile) 
+  system(cmd) 
+  results = read.table(tempfile,header=FALSE,sep="\t") 
+  
+  # Delete temp files
+  deleteTempfiles(c(tempfile,i[[2]],files[[2]]))
+  
+  return (results)
+}
 
-            if (!is.character(i) && !is.numeric(i)) {
-            iTable = paste0(tempdir(), "/iTable.txt")
-            write.table(i, iTable, append = "FALSE", sep = "	", quote = FALSE, col.names = FALSE, row.names = FALSE) 
-            i=iTable } 
-            
-            if (!is.character(files) && !is.numeric(files)) {
-            filesTable = paste0(tempdir(), "/filesTable.txt")
-            write.table(files, filesTable, append = "FALSE", sep = "	", quote = FALSE, col.names = FALSE, row.names = FALSE) 
-            files=filesTable } 
-            
-		options = "" 
- 
-            if (!is.null(S)) {
-            options = paste(options," -S")
-            if(is.character(S) || is.numeric(S)) {
-            options = paste(options, " ", S)
-            }   
-            }
-             
-            if (!is.null(both)) {
-            options = paste(options," -both")
-            if(is.character(both) || is.numeric(both)) {
-            options = paste(options, " ", both)
-            }   
-            }
-             
-            if (!is.null(counts)) {
-            options = paste(options," -counts")
-            if(is.character(counts) || is.numeric(counts)) {
-            options = paste(options, " ", counts)
-            }   
-            }
-             
-            if (!is.null(s)) {
-            options = paste(options," -s")
-            if(is.character(s) || is.numeric(s)) {
-            options = paste(options, " ", s)
-            }   
-            }
-             
-            if (!is.null(names)) {
-            options = paste(options," -names")
-            if(is.character(names) || is.numeric(names)) {
-            options = paste(options, " ", names)
-            }   
-            }
-            
-	# establish output file 
-	tempfile = tempfile("bedtoolsr", fileext=".txt")
-	bedtools.path <- getOption("bedtools.path")
-	if(!is.null(bedtools.path)) bedtools.path <- paste0(bedtools.path, "/")
-	cmd = paste0(bedtools.path, "bedtools annotate ", options, " -i ", i, " -files ", files, " > ", tempfile) 
-	system(cmd) 
-	results = read.table(tempfile,header=FALSE,sep="\t") 
-        if (file.exists(tempfile)){ 
-        file.remove(tempfile) 
-        }
-        return (results)
-        }
-         
-        if(exists("iTable")) { 
-        file.remove (iTable)
-        } 
- 
-        if(exists("filesTable")) { 
-        file.remove (filesTable)
-        } 
+
+
+
+
