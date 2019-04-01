2e447bef79d6cc226e83dafbe35c48f29ed09523
Craig Wenger <craig.wenger@gmail.com>
2019-04-01 16:03:00 -0400

    Remove unused part of previous code.

--- dev/zzz.R
+++ dev/zzz.R
@@ -1,24 +1,21 @@
 .onLoad <- function(libname, pkgname) {
-  if(length(row)>0)
+  bedtoolsr_version<-utils::packageVersion("bedtoolsr")
+  response<-tryCatch(
   {
-    bedtoolsr_version<-utils::packageVersion("bedtoolsr")
-    response<-tryCatch(
-    {
-      bedtools.path <- getOption("bedtools.path")
-      if(!is.null(bedtools.path)) bedtools.path <- paste0(bedtools.path, "/")
-      system(paste0(bedtools.path, "bedtools --version"), intern=TRUE)
-    },
-    error = function(e)
-    {
-      warning("bedtools does not appear to be installed or not in your PATH. If it is installed, please add it to your PATH or run:\noptions(bedtools.path = \\\"[bedtools path]\\\")")
-      return(NULL)
-    })
-    if(!is.null(response)) {
-      installed_bedtools_version<-substr(response, 11, nchar(response))
-      package_bedtools_version<-substr(bedtoolsr_version, 1, nchar(installed_bedtools_version))
-      if(installed_bedtools_version != package_bedtools_version) {
-        warning(paste("bedtoolsr was built with bedtools version", package_bedtools_version, "but you have version", installed_bedtools_version, "installed. Function syntax may have changed and wrapper will not function correctly. To fix this, please install bedtools version", package_bedtools_version, "and either add it to your PATH or run:\noptions(bedtools.path = \\\"[bedtools path]\\\")"))
-      }
+    bedtools.path <- getOption("bedtools.path")
+    if(!is.null(bedtools.path)) bedtools.path <- paste0(bedtools.path, "/")
+    system(paste0(bedtools.path, "bedtools --version"), intern=TRUE)
+  },
+  error = function(e)
+  {
+    warning("bedtools does not appear to be installed or not in your PATH. If it is installed, please add it to your PATH or run:\noptions(bedtools.path = \\\"[bedtools path]\\\")")
+    return(NULL)
+  })
+  if(!is.null(response)) {
+    installed_bedtools_version<-substr(response, 11, nchar(response))
+    package_bedtools_version<-substr(bedtoolsr_version, 1, nchar(installed_bedtools_version))
+    if(installed_bedtools_version != package_bedtools_version) {
+      warning(paste("bedtoolsr was built with bedtools version", package_bedtools_version, "but you have version", installed_bedtools_version, "installed. Function syntax may have changed and wrapper will not function correctly. To fix this, please install bedtools version", package_bedtools_version, "and either add it to your PATH or run:\noptions(bedtools.path = \\\"[bedtools path]\\\")"))
     }
   }
 }
